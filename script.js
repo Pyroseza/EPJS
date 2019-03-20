c09f6a9186ae88b228fa4da468069d963717304d
Mark Joling <markjioling3@gmail.com>
2019-03-20 11:28:11 +0100

    added script.js, moved js out of the html

---
+++ script.js
@@ -0,0 +1,48 @@
+(function () {
+    // Retrieve remote BrowserWindow
+    const {BrowserWindow} = require('electron').remote
+
+    // keep track of the dev status
+    let dev = false
+
+    function init() {
+        // Minimize task
+        document.getElementById("dev").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            if (dev === false) {
+                window.webContents.openDevTools()
+                dev = true
+            }else{
+                window.webContents.closeDevTools()
+                dev = false
+            }
+            console.log("dev tools:", dev)
+        })
+        document.getElementById("min-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            window.minimize();
+        });
+
+        // Maximize window
+        document.getElementById("max-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            if (window.isMaximized()) {
+                window.unmaximize();
+            } else {
+                window.maximize();
+            }
+        });
+
+        // Close app
+        document.getElementById("close-btn").addEventListener("click", (e) => {
+            var window = BrowserWindow.getFocusedWindow();
+            window.close();
+        });
+    };
+
+    document.onreadystatechange = () => {
+        if (document.readyState == "complete") {
+            init();
+        }
+    };
+})();
